48b7e7065ece51b677e9b6ad177ad86c7a0a3a7e
Muhammad Azeez <muhammad-azeez@outlook.com>
2018-03-08 18:10:01 +0300

    header and footer

---
+++ header.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <title><?= isset($PageTitle) ? $PageTitle : "Erbil Polytechnic University"?></title>
+    <!-- Additional tags here -->
+    <?php if (function_exists('customPageHeader')){
+      customPageHeader();
+    }?>
+
+  <!-- Bootstrap core CSS -->
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/site.css" rel="stylesheet">
+</head>
+
+<body>
+
+  <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
+    <h5 class="my-0 mr-md-auto font-weight-normal">Erbil Polytechnic University</h5>
+    <nav class="my-2 my-md-0 mr-md-3">
+      <a class="p-2 text-dark" href="#">Students</a>
+      <a class="p-2 text-dark" href="#">Employees</a>
+      <a class="p-2 text-dark" href="#">Subjects</a>
+      <a class="p-2 text-dark" href="#">Users</a>
+    </nav>
+    <a class="btn btn-outline-primary" href="#">Sign up</a>
+  </div>
--- index.php
+++ index.php
@@ -1,9 +1,57 @@
-<html>
-    <head>
+<?php include('header.php') ?>
 
-    </head>
+<div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+  <h1 class="display-4">Erbil Polytechnic University</h1>
+  <p class="lead">Erbil Polytechnic University is a public polytechnic university in Erbil, Kurdistan founded in 1993.</p>
+  <img class="img-fluid" src="images\cover.jpg">
+</div>
 
-    <body>
+<div class="container">
+  <div class="card-deck mb-3">
+    <div class="card mb-4 box-shadow">
+      <div class="card-header">
+        <h4 class="my-0 font-weight-normal">Engineering College</h4>
+      </div>
+      <div class="card-body">
+        <p>Engineering College includes these departments:</p>
+        <ul class="mt-3 mb-4">
+          <li>Civil</li>
+          <li>Highway</li>
+          <li>Information Systems</li>
+          <li>Mechanic and Energy</li>
+        </ul>
+        <button type="button" class="btn btn-lg btn-block btn-primary">Visit</button>
+      </div>
+    </div>
+    <div class="card mb-4 box-shadow">
+      <div class="card-header">
+        <h4 class="my-0 font-weight-normal">Administrative College</h4>
+      </div>
+      <div class="card-body">
+        <p>Administrative College is another college of EPU. It includes these departments:</p>
+        <ul class="mt-3 mb-5">
+          <li>Accounting department</li>
+          <li>Adminstration department</li>
+        </ul>
 
-    </body>
-</html>
+        <button type="button" class="btn btn-lg btn-block btn-primary">Visit</button>
+      </div>
+    </div>
+    <div class="card mb-4 box-shadow">
+      <div class="card-header">
+        <h4 class="my-0 font-weight-normal">Health college</h4>
+      </div>
+      <div class="card-body">
+        <p>Engineering College includes these departments:</p>
+        <ul class="mt-3 mb-4">
+          <li>Laboratory Department</li>
+          <li>Psychiatric department</li>
+          <li>Department of Neuroscience</li>
+          <li>Nursing department</li>
+        </ul>
+        <button type="button" class="btn btn-lg btn-block btn-primary">Visit</button>
+      </div>
+    </div>
+  </div>
+
+<?php include('footer.php') ?>
