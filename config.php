48a55bc3fdf6790c2708a34afd4b6a37846be64f
encryptor <encryptor@outlook.com>
2018-03-08 11:08:19 -0500

    Fix passwords

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
 $databaseHost = 'localhost';
-$databaseName = 'test';
-$databaseUsername = getenv('DB_USERNAME');
-$databasePassword = getenv('DB_PASSWORD');
+$databaseName = 'Skool';
+// Define $databaseUsername in password.php
+// Define $databasePassword in password.php
+require('password.php');
 $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
- 
 ?>
--- header.php
+++ header.php
@@ -19,7 +19,7 @@
 </head>
 
 <body>
-
+  <?php require_once('config.php') ?>
   <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
     <h5 class="my-0 mr-md-auto font-weight-normal">Erbil Polytechnic University</h5>
     <nav class="my-2 my-md-0 mr-md-3">
