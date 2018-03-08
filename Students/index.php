280d6c7a910bad69ffcc8340f884abfadde62976
encryptor <encryptor@outlook.com>
2018-03-08 12:55:19 -0500

    Datatables

--- Students/index.php
+++ Students/index.php
@@ -6,27 +6,39 @@
 ?>
 
 <div class="container">
-  <a href="add.php">Add</a>
-  <table class="table">
-    <thead>
-        <tr>
-            <th scope="col">Name</th>
-            <th scope="col">Stage</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php
-        while($row = mysqli_fetch_array($result))
-        {
-            echo '
-            <tr>
-                <td>'. $row['StudentName'] .'</th>
-                <td>'. $row['StudentStage'] .'</td>
-            </tr>
-            ';
-        }
-    ?>
-    </tbody>
-</table>
+  <a href="add.php">Add Student</a>
+  <hr>
+  <div class="row">
+    <table id="table" class="table table-striped">
+      <thead>
+          <tr>
+              <th scope="col">Name</th>
+              <th scope="col">Stage</th>
+              <th scope="col">Action</th>
+          </tr>
+      </thead>
+      <tbody>
+          <?php
+          while($row = mysqli_fetch_array($result))
+          {
+              echo '
+              <tr>
+                  <td>'. $row['StudentName'] .'</th>
+                  <td>'. $row['StudentStage'] .'</td>
+                  <td class="mx-auto text-center">
+                  <button type="button" class="btn btn-danger delete-button">Delete</button>
+                  </td> 
+              </tr>
+              ';
+          }
+      ?>
+      </tbody>
+    </table>
+  </div>
+<script>
+  $(document).ready(function() {
+     $("#table").DataTable();             
+  });
+</script>
 
 <?php include('../footer.php') ?>
